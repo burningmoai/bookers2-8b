157c7e1a1a9cdba9ccba8752e72c73b544295312
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-02 08:37:01 +0000

    [Add]フォロー/フォロワー一覧画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,15 @@ class UsersController < ApplicationController
     end
   end
 
-  def followings
-    user = User.find(params[:id])
-    @users = user.followings
-  end
+  # def followings
+    # user = User.find(params[:id])
+    # @users = user.followings
+  # end
 
-  def followers
-    user = User.find(params[:id])
-    @users = user.followers
-  end
+  # def followers
+    # user = User.find(params[:id])
+    # @users = user.followers
+  # end
 
   private
 

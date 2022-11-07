613d2883209c6b75d08b7392e2f2ce723af8218d
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-07 12:43:21 +0000

    [Finish]応用課題6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,19 @@
 class PostCommentsController < ApplicationController
 
   def create
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(post_comment_params)
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     PostComment.find(params[:id]).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 private

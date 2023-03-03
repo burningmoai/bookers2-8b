795a1deca8af71588eb7ee6c5ea9d3a30f16cc28
burningmoai <114802516+burningmoai@users.noreply.github.com>
2023-03-03 14:51:03 +0000

    [Add]モデルの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # relationships=follower_idが同じのものをとってくるので、フォローされた人のidの集まりである

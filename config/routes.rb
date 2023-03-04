cdb7cd0bc16b97aad15fada7f5464b2984bfb344
burningmoai <114802516+burningmoai@users.noreply.github.com>
2023-03-04 14:28:26 +0000

    [Add]userコントローラshowアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
     get 'search_date' => 'users#search_date'
+    resources :messages, only: [:create]
+    resources :rooms, only: [:create, :index, :show]
   end
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update,:new] do
     resource :favorites, only: [:create, :destroy]
